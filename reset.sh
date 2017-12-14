adc554ac4cc856595d0c07196502d62b900627e7
Scott Stanfield <scottstanfield@gmail.com>
2017-12-13 17:05:13 -0800

    Using find instead of glob

--- reset.sh
+++ reset.sh
@@ -1,4 +1,4 @@
 rm -rf {errorlogs,hold,import,output,processed,review}
 ./initial.setup.sh
-cp ~/fec/daily.fec/1177*.fec import
+find ~/fec/daily.fec -type f -newermt "2016-01-01" ! -newermt "2017-01-01" -exec cp -a {} import \;
 rm log
